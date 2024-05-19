8132034e6f56f195b8e7eb106a898da75fa773fc
fnursaray <fatmanursaray@icloud.com>
2024-05-19 14:57:03 +0300

    finish

---
+++ data.js
@@ -0,0 +1,32 @@
+const songsList = [
+    {
+        name: "Crying In The Rain",
+        link: "mp3/1.mp3",
+        artist: "FT Island",
+        image: "images/1.jpeg"
+    },
+    {
+        name: "Untinkable",
+        link: "mp3/2.mp3",
+        artist: "FT Island",
+        image: "images/2.jpeg"
+    },
+    {
+        name: "Wind",
+        link: "mp3/3.mp3",
+        artist: "FT Island",
+        image: "images/3.webp"
+    },
+    {
+        name: "To The Light",
+        link: "mp3/4.mp3",
+        artist: "FT Island",
+        image: "images/4.jpg"
+    },
+    {
+        name: "Morning Coffee",
+        link: "mp3/5.mp3",
+        artist: "FT Island",
+        image: "images/5.jpg"
+    }
+]
